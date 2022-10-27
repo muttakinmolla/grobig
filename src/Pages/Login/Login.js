e103f476e3831408523ce897bcc5bf1de8965d28
muttakin1991 <shawonmuttakin@gmail.com>
2022-10-27 22:32:43 +0600

    some modified

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 setError('');
-                toast.success();
+                toast.success('successfully login');
                 navigate(from, { replace: true })
             })
             .catch(error => {
